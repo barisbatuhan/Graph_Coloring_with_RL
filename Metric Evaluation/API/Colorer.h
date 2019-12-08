46d166c9756451f44beff49427ba87634cc031e8
berkerdemirel <berkerdemirel@sabanciuniv.edu>
2019-12-08 23:05:29 +0300

    distance 2 coloring added

--- Metric Evaluation/API/Colorer.h
+++ Metric Evaluation/API/Colorer.h
@@ -61,6 +61,43 @@ int Colorer::colorGreedily(vector<pair<int, float>> & ordering, int maxDegree) {
 	return nofcolors;
 }
 
+int Colorer::dist2ColorGreedily(vector<pair<int, float>> & ordering, int maxDegree) {
+	vector<int> color_arr;
+	color_arr.resize(ordering.size(), -1);
+	int nofcolors = 0;
+	vector<int> forbid_arr(maxDegree + 1, -1);
+	bool hasEdge = false;
+	for (int i = 0; i<ordering.size(); i++) {
+		const int & node = ordering[i].first; //for each node in ordering
+		for (int edge = row_ptr[node]; edge < row_ptr[node + 1]; edge++) {
+			hasEdge = true;
+			const int & adj = col_ind[edge];//for each adjacent node
+			for (int edge_2 = row_ptr[adj]; edge_2 < row_ptr[adj + 1]; edge_2++) {
+				const int & adj_neigh = col_ind[edge_2];
+				if (color_arr[adj_neigh] != -1) { // if it is already colored
+					forbid_arr[color_arr[adj_neigh]] = node; // that color is forbidden to node
+				}
+			}
+		}
+		for (int color = 0; color < maxdegree; color++) { // greedily choose the smallest possible color
+			if (forbid_arr[color] != node) {
+				color_arr[node] = color;
+				if (nofcolors < color) {
+					nofcolors = color;
+				}
+				break;
+			}
+		}
+	}
+	if (hasEdge) {
+		nofcolors++;
+	}
+	if (!isValid(color_arr) == true) {
+		cout << "The Coloring made by colorGreedily function is not valid!" << endl;
+	}
+	return nofcolors;
+}
+
 bool Colorer::isValid(const vector<int> & colorArr) {
 	for (int v = 0; v < nodeSize; v++) { // for each node v
 		for (int e = rowPtr[v]; e < rowPtr[v + 1]; e++) {
