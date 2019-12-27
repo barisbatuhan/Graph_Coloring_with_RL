9e48a58688417a4f5aacfed743e9c647765df44f
barisbatuhan <barisbatuhantopal@gmail.com>
2019-12-27 14:08:53 +0300

    main is uncommented

--- Metric Evaluation/main.cpp
+++ Metric Evaluation/main.cpp
@@ -358,181 +358,181 @@ void normalize(vector<vector <pair<int, float> > > & orders){
 	}
 }
 
-// int main(int argc, char** argv) {
+int main(int argc, char** argv) {
 
-// 	const char* path = argv[1];
-// 	DIR *pDIR;
-// 	struct dirent *entry;
-// 	map<string, int> ht;
-// 	/*
-// 	const char* chrom = "chromatic_numbers.csv";
-// 	ifstream f(chrom);
-// 	string graphname;
-// 	int chronum;
-// 	getline(f, graphname);
+	const char* path = argv[1];
+	DIR *pDIR;
+	struct dirent *entry;
+	map<string, int> ht;
+	/*
+	const char* chrom = "chromatic_numbers.csv";
+	ifstream f(chrom);
+	string graphname;
+	int chronum;
+	getline(f, graphname);
 
-// 	while(!f.eof()){
-// 		getline(f, graphname);
-// 		if(graphname.length()<2){
-// 			break;
-// 		}
-// 		graphname = graphname.substr(0, graphname.rfind(","));
-// 		string d = graphname.substr(graphname.rfind(",")+1, graphname.length());
-// 		graphname = graphname.substr(0, graphname.find(","));
-// 		chronum = stoi(d);
-// 		ht.insert(pair<string,int>(graphname, chronum));
-// 	}
-// 	*/
-// 	vector<string> gnames;
-// 	vector<pair<vector<int>, vector<int> > > graphs;
-// 	if (pDIR = opendir(path)) {
-// 		while (entry = readdir(pDIR)) {
-// 			//cout << entry->d_name << endl;
-// 			string g = entry->d_name;
-// 			string fname = path + ((string)entry->d_name);
-// 			if (fname.at(fname.length() - 1) == '.') {
-// 				continue;
-// 			}
-// 			cout << g << endl;
-// 			gnames.push_back(g);
+	while(!f.eof()){
+		getline(f, graphname);
+		if(graphname.length()<2){
+			break;
+		}
+		graphname = graphname.substr(0, graphname.rfind(","));
+		string d = graphname.substr(graphname.rfind(",")+1, graphname.length());
+		graphname = graphname.substr(0, graphname.find(","));
+		chronum = stoi(d);
+		ht.insert(pair<string,int>(graphname, chronum));
+	}
+	*/
+	vector<string> gnames;
+	vector<pair<vector<int>, vector<int> > > graphs;
+	if (pDIR = opendir(path)) {
+		while (entry = readdir(pDIR)) {
+			//cout << entry->d_name << endl;
+			string g = entry->d_name;
+			string fname = path + ((string)entry->d_name);
+			if (fname.at(fname.length() - 1) == '.') {
+				continue;
+			}
+			cout << g << endl;
+			gnames.push_back(g);
 
-// 			//cout << fname << endl;
-// 			int num_nodes, num_edges;
-// 			vector<int> row_ptr, col_ind;
-// 			if (read_graphs(fname, num_nodes, num_edges, row_ptr, col_ind, graphs) == -1) {
-// 				cerr << "error reading graph" << endl;
-// 				return 0;
-// 			}
+			//cout << fname << endl;
+			int num_nodes, num_edges;
+			vector<int> row_ptr, col_ind;
+			if (read_graphs(fname, num_nodes, num_edges, row_ptr, col_ind, graphs) == -1) {
+				cerr << "error reading graph" << endl;
+				return 0;
+			}
 
-// 			//baseline
-// 			vector <pair<int, float> > orders(num_nodes);
-// 			vector<int> color_arr;
-// 			degree_order(num_nodes, row_ptr, col_ind, orders);
-// 			int maxdegree = INT_MIN;
-// 			for(auto & x: orders){
-// 				if(x.second>maxdegree){
-// 					maxdegree = x.second;
-// 				}
-// 			}
-// 			//degree_2_order(num_nodes, row_ptr, col_ind, orders);
-// 			sort(orders.begin(), orders.end(), descending);
-// 			int k = graph_coloring(row_ptr, col_ind, orders, color_arr, maxdegree);
-// 			ht.insert(pair<string,int>(g,k));
-//     }
-// 		closedir(pDIR);
-// 	}
-// 	float sum;
+			//baseline
+			vector <pair<int, float> > orders(num_nodes);
+			vector<int> color_arr;
+			degree_order(num_nodes, row_ptr, col_ind, orders);
+			int maxdegree = INT_MIN;
+			for(auto & x: orders){
+				if(x.second>maxdegree){
+					maxdegree = x.second;
+				}
+			}
+			//degree_2_order(num_nodes, row_ptr, col_ind, orders);
+			sort(orders.begin(), orders.end(), descending);
+			int k = graph_coloring(row_ptr, col_ind, orders, color_arr, maxdegree);
+			ht.insert(pair<string,int>(g,k));
+    }
+		closedir(pDIR);
+	}
+	float sum;
 
-// 	vector<vector<float> > coefficients(32,vector<float>(6));
-// 	vector<float> min_colors(32, INT_MAX);
-// 	vector<float> coef_list(21);
-// 	for(int i=0; i<=20; i++){coef_list[i] = i*0.05;}
-// 	cout << "it begins" << endl;
+	vector<vector<float> > coefficients(32,vector<float>(6));
+	vector<float> min_colors(32, INT_MAX);
+	vector<float> coef_list(21);
+	for(int i=0; i<=20; i++){coef_list[i] = i*0.05;}
+	cout << "it begins" << endl;
 
 
 
-// 	//#pragma omp parallel for collapse(6) schedule(dynamic) num_threads(6)
-// 	for(int i1 = 0; i1 < 21; i1 +=20){
-// 		for(int i2 = 0; i2 < 21; i2+=20){
-// 			for(int i3 = 0; i3 < 21; i3+=20){
-// 				for(int i4 = 0; i4 < 21; i4 +=20){
-// 					for(int i5 = 0; i5 < 21; i5+=20){
-// 						for(int i6 = 0; i6 < 21; i6+=20){
-// 								float c1,c2,c3,c4,c5,c6;
-// 								c1 = coef_list[i1];
-// 								c2 = coef_list[i2];
-// 								c3 = coef_list[i3];
-// 								c4 = coef_list[i4];
-// 								c5 = coef_list[i5];
-// 								c6 = coef_list[i6];
-// 								sum = c1+c2+c3+c4+c5+c6;
-// 								if(sum== 1){
-// 									printf("%f %f %f %f %f %f\n", c1 ,c2 ,c3, c4, c5, c6);
-// 									float total_colors = 0;
-// 									int tid = omp_get_thread_num();
-// 									int i = 0;
-// 									for(auto & graph: graphs){
-// 										auto & row_ptr = graph.first;
-// 										auto & col_ind = graph.second;
-// 										int num_nodes = row_ptr.size()-1;
-// 										vector<vector <pair<int, float> > > orders(6, vector<pair<int,float> >(num_nodes));
-// 										degree_order(num_nodes, row_ptr, col_ind, orders[0]);
-// 										cout << "degree ordered" << endl;
-// 										int maxdegree = INT_MIN;
-// 										for(auto & x: orders[0]){
-// 											if(x.second>maxdegree){
-// 												maxdegree = x.second;
-// 											}
-// 										}
-// 										degree_2_order(num_nodes, row_ptr, col_ind, orders[1]);
-// 										cout << "degree2 ordered" << endl;
-// 										degree_3_order(num_nodes, row_ptr, col_ind, orders[2]);
-// 										cout << "degree3 ordered" << endl;
-// 										closeness_centrality_approx(num_nodes, row_ptr, col_ind, orders[3]);
-// 										cout << "closeness ordered" << endl;
-// 										clustering_coeff(num_nodes, row_ptr, col_ind, orders[4]);
-// 										cout << "clustering ordered" << endl;
-// 										page_rank(num_nodes, row_ptr, col_ind, orders[5]);
-// 										cout << "pagerank ordered" << endl;
+	//#pragma omp parallel for collapse(6) schedule(dynamic) num_threads(6)
+	for(int i1 = 0; i1 < 21; i1 +=20){
+		for(int i2 = 0; i2 < 21; i2+=20){
+			for(int i3 = 0; i3 < 21; i3+=20){
+				for(int i4 = 0; i4 < 21; i4 +=20){
+					for(int i5 = 0; i5 < 21; i5+=20){
+						for(int i6 = 0; i6 < 21; i6+=20){
+								float c1,c2,c3,c4,c5,c6;
+								c1 = coef_list[i1];
+								c2 = coef_list[i2];
+								c3 = coef_list[i3];
+								c4 = coef_list[i4];
+								c5 = coef_list[i5];
+								c6 = coef_list[i6];
+								sum = c1+c2+c3+c4+c5+c6;
+								if(sum== 1){
+									printf("%f %f %f %f %f %f\n", c1 ,c2 ,c3, c4, c5, c6);
+									float total_colors = 0;
+									int tid = omp_get_thread_num();
+									int i = 0;
+									for(auto & graph: graphs){
+										auto & row_ptr = graph.first;
+										auto & col_ind = graph.second;
+										int num_nodes = row_ptr.size()-1;
+										vector<vector <pair<int, float> > > orders(6, vector<pair<int,float> >(num_nodes));
+										degree_order(num_nodes, row_ptr, col_ind, orders[0]);
+										cout << "degree ordered" << endl;
+										int maxdegree = INT_MIN;
+										for(auto & x: orders[0]){
+											if(x.second>maxdegree){
+												maxdegree = x.second;
+											}
+										}
+										degree_2_order(num_nodes, row_ptr, col_ind, orders[1]);
+										cout << "degree2 ordered" << endl;
+										degree_3_order(num_nodes, row_ptr, col_ind, orders[2]);
+										cout << "degree3 ordered" << endl;
+										closeness_centrality_approx(num_nodes, row_ptr, col_ind, orders[3]);
+										cout << "closeness ordered" << endl;
+										clustering_coeff(num_nodes, row_ptr, col_ind, orders[4]);
+										cout << "clustering ordered" << endl;
+										page_rank(num_nodes, row_ptr, col_ind, orders[5]);
+										cout << "pagerank ordered" << endl;
 
-// 										normalize(orders);
-// 										cout << "normalized" << endl;
+										normalize(orders);
+										cout << "normalized" << endl;
 
-// 										vector<int> color_arr;
-// 										vector<pair<int, float> > new_order(num_nodes);
-// 										for(int i=0; i<num_nodes; i++){
-// 											new_order[i] = make_pair(i,c1*orders[2][i].second + c2*orders[1][i].second + c3*orders[0][i].second + c4*orders[3][i].second + c5*orders[4][i].second + c6*orders[5][i].second);
-// 											//new_order[i] = make_pair(i,0.1*orders[2][i].second + 0*orders[1][i].second + 0.15*orders[0][i].second + 0.7*orders[3][i].second + 0.05*orders[4][i].second + 0*orders[5][i].second);
-// 											//new_order[i] = make_pair(i, orders[1][i].second);
-// 										}
+										vector<int> color_arr;
+										vector<pair<int, float> > new_order(num_nodes);
+										for(int i=0; i<num_nodes; i++){
+											new_order[i] = make_pair(i,c1*orders[2][i].second + c2*orders[1][i].second + c3*orders[0][i].second + c4*orders[3][i].second + c5*orders[4][i].second + c6*orders[5][i].second);
+											//new_order[i] = make_pair(i,0.1*orders[2][i].second + 0*orders[1][i].second + 0.15*orders[0][i].second + 0.7*orders[3][i].second + 0.05*orders[4][i].second + 0*orders[5][i].second);
+											//new_order[i] = make_pair(i, orders[1][i].second);
+										}
 
-// 										sort(new_order.begin(), new_order.end(), descending);
-// 										float k = graph_coloring(row_ptr, col_ind, new_order, color_arr, maxdegree);
-// 										float d = ht[gnames[i]];
-// 										//cout << k << " " << d << endl;
-// 										if(d == 0){
-// 											continue;
-// 										}
-// 										printf("%d, %d\n", k,d);
-// 										total_colors += k/d;
-// 										i++;
-// 									}
-// 									total_colors = total_colors / (i);
-// 									printf("%f\t%f %f %f %f %f %f\n", total_colors, c1 ,c2 ,c3, c4, c5, c6);
-// 									if(total_colors < min_colors[tid]){
-// 										coefficients[tid][0] = c1;
-// 										coefficients[tid][1] = c2;
-// 										coefficients[tid][2] = c3;
-// 										coefficients[tid][3] = c4;
-// 										coefficients[tid][4] = c5;
-// 										coefficients[tid][5] = c6;
-// 										printf("Total colors : %f\n", total_colors);
-// 										min_colors[tid] = total_colors;
-// 									}
-// 								}
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
+										sort(new_order.begin(), new_order.end(), descending);
+										float k = graph_coloring(row_ptr, col_ind, new_order, color_arr, maxdegree);
+										float d = ht[gnames[i]];
+										//cout << k << " " << d << endl;
+										if(d == 0){
+											continue;
+										}
+										printf("%d, %d\n", k,d);
+										total_colors += k/d;
+										i++;
+									}
+									total_colors = total_colors / (i);
+									printf("%f\t%f %f %f %f %f %f\n", total_colors, c1 ,c2 ,c3, c4, c5, c6);
+									if(total_colors < min_colors[tid]){
+										coefficients[tid][0] = c1;
+										coefficients[tid][1] = c2;
+										coefficients[tid][2] = c3;
+										coefficients[tid][3] = c4;
+										coefficients[tid][4] = c5;
+										coefficients[tid][5] = c6;
+										printf("Total colors : %f\n", total_colors);
+										min_colors[tid] = total_colors;
+									}
+								}
+						}
+					}
+				}
+			}
+		}
+	}
 
-// 	float mincolors = INT_MAX;
-// 	int tid =-1;
-// 	for(int i=0; i<min_colors.size(); i++){
-// 		if(min_colors[i]<mincolors){
-// 			mincolors = min_colors[i];
-// 			tid = i;
-// 		}
-// 	}
-// 	cout << "best result is " << mincolors << endl;
-// 	cout << "Coefficients are ";
-// 	for(auto & x: coefficients[tid]){
-// 		cout << x << " ";
-// 	}
-// 	cout << endl;
+	float mincolors = INT_MAX;
+	int tid =-1;
+	for(int i=0; i<min_colors.size(); i++){
+		if(min_colors[i]<mincolors){
+			mincolors = min_colors[i];
+			tid = i;
+		}
+	}
+	cout << "best result is " << mincolors << endl;
+	cout << "Coefficients are ";
+	for(auto & x: coefficients[tid]){
+		cout << x << " ";
+	}
+	cout << endl;
 
-// 	return 0;
-// }
+	return 0;
+}
 
 
